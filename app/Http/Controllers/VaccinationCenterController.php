ad3e5cf143e11fe4ebaa011e90f758c0ec0ac412
Md Rasel <raselbabu34.bd@gmail.com>
2021-08-15 16:33:38 +0600

    database design

---
+++ app/Http/Controllers/VaccinationCenterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\vaccination_center;
+use Illuminate\Http\Request;
+
+class VaccinationCenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\vaccination_center  $vaccination_center
+     * @return \Illuminate\Http\Response
+     */
+    public function show(vaccination_center $vaccination_center)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\vaccination_center  $vaccination_center
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(vaccination_center $vaccination_center)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\vaccination_center  $vaccination_center
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, vaccination_center $vaccination_center)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\vaccination_center  $vaccination_center
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(vaccination_center $vaccination_center)
+    {
+        //
+    }
+}
